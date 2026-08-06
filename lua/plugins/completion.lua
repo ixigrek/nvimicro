201263abfad2b2ca00e1a016ae20777bfab8a8ff
ixigrek <simon.jaricot@protonmail.com>
2026-08-06 11:07:49 +0200

    feat: add blink.cmp completion

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,17 @@
+-- lua/plugins/completion.lua
+return {
+  "saghen/blink.cmp",
+  event = "InsertEnter",
+  version = "*",
+  opts = {
+    keymap = { preset = "default" },
+    appearance = { nerd_font_variant = "mono" },
+    completion = {
+      documentation = { auto_show = true, auto_show_delay_ms = 200 },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+  },
+  opts_extend = { "sources.default" },
+}
